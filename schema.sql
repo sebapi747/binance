d142ba553cde58869cc844f75150faefc2fd5ce8
sebapi747 <sebapi747@github.com>
2021-05-16 22:24:22 +0800

    add table for futures

--- schema.sql
+++ schema.sql
@@ -1,4 +1,9 @@
-create table quotes (
+/*create table quotes (
 symbol text, price numeric,dt datetime,
 primary key(symbol, dt)
+);*/
+
+create table futures (
+symbol text, ps text, price numeric,time numeric,
+primary key(symbol, time)
 );
