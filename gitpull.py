99ef2b47d850a9cb434650e7a9d5b457f437a6dc
seb <seb@hermes.com>
2024-05-22 11:42:35 +0800

    skip non sebapi git

--- gitpull.py
+++ gitpull.py
@@ -7,4 +7,11 @@ for d in os.listdir(".."):
     if os.path.isdir(otherdir) and os.path.exists(otherdir+"/.git"):
         print(otherdir)
         os.chdir(otherdir)
+        os.system("git remote --v > gitremote.log")
+        f = open("gitremote.log")
+        line = f.readline()
+        f.close()
+        if "sebapi747" not in line:
+            print("INFO: skipping " + line)
+            continue
         os.system("git pull --rebase")
