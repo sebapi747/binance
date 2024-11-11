83c4039779aaf9116b23caec62e3e735b20e10f7
seb <sebastien.hitier@gmail.com>
2024-11-12 00:49:01 +0800

    initial commit

---
+++ heartbeat.py
@@ -0,0 +1,24 @@
+import os
+import requests
+import config
+filedir = os.path.dirname(__file__)
+os.chdir("./" if filedir=="" else filedir)
+
+def sendTelegram(text):
+    prefix = os.uname()[1] + __file__ + ":"
+    params = {'chat_id': config.telegramchatid, 'text': prefix+text, 'parse_mode': 'HTML'}
+    resp = requests.post('https://api.telegram.org/bot{}/sendMessage'.format(config.telegramtoken), params)
+    resp.raise_for_status()
+
+def checkheartbeat():
+    for h in ["gaia","themis","darkstar","hermes"]:
+        os.system("host %s > resolved.%s" % (h,h))
+        with open("resolved.%s" % h,"r") as f:
+            line = f.readline()
+            if "address" in line:
+                print("INFO:",line)
+            else:
+                print("ERR:",line)
+                sendTelegram("%s not foun amongst hosts" % h)
+                        
+checkheartbeat()
